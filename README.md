4bdcffe12f27ee5fc5659b01cbdd752a492a0832
Alan6 <liujian81@gmail.com>
2016-01-17 12:41:14 -0500

    add project

---
+++ README.md
@@ -0,0 +1,8 @@
+#Restaurant API
+This RESTful API allows user to find restaurants by specifying its location and meal type, and add to database for future use.<br>
+It returns the query result as JSON. It also supports CREATE/UPDATE/DELETE functionalities.<br>
+Queried restaurant data from Foursquare is saved in SQLITE.
+
+Written in <b>Python, Flask</b><br>
+Database: <b>SQLITE, sqlalchemy</B><br>
+APIs Used: Google Maps API and Foursquare API  
---
+++ findARestaurant.py
@@ -0,0 +1,50 @@
+import json
+import httplib2
+import os
+import sys
+import codecs
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+sys.stderr = codecs.getwriter('utf8')(sys.stderr)
+#Environment varibles were configuered on the ALAN-PC labtop
+foursquare_client_id = os.environ.get('FOURSQUARE_ID')
+foursquare_client_secret = os.environ.get('FOURSQUARE_SECRET')
+google_api_key = os.environ.get('GOOGLE_API_KEY')
+#
+def getGeocodeLocation(inputString):
+    locationString = inputString.replace(" ", "+")
+    url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
+    h = httplib2.Http()
+    result = json.loads(h.request(url,'GET')[1])
+    latitude = result['results'][0]['geometry']['location']['lat']
+    longitude = result['results'][0]['geometry']['location']['lng']
+    return (latitude,longitude)
+
+def findARestaurant(mealType, location):
+    latitude, longitude = getGeocodeLocation(location)
+    url = ('https://api.foursquare.com/v2/venues/search?client_id=%s&client_secret=%s&v=20130815&ll=%s,%s&query=%s' % (foursquare_client_id, foursquare_client_secret,latitude,longitude,mealType))
+    h = httplib2.Http()
+    result = json.loads(h.request(url,'GET')[1])
+    if result['response']['venues']:
+        restaurant = result['response']['venues'][0]
+        venue_id = restaurant['id']
+        restaurant_name = restaurant['name']
+        restaurant_address = restaurant['location']['formattedAddress']
+        address = ""
+        for i in restaurant_address:
+            address += i + " "
+        restaurant_address = address
+
+        url = ('https://api.foursquare.com/v2/venues/%s/photos?client_id=%s&v=20150603&client_secret=%s' % ((venue_id,foursquare_client_id,foursquare_client_secret)))
+        result = json.loads(h.request(url,'GET')[1])
+
+        if result['response']['photos']['items']:
+            firstpic = result['response']['photos']['items'][0]
+            prefix = firstpic['prefix']
+            suffix = firstpic['suffix']
+            imageURL = prefix + "300x300" + suffix
+        else:
+            imageURL = "http://pixabay.com/get/8926af5eb597ca51ca4c/1433440765/cheeseburger-34314_1280.png?direct"
+            restaurantInfo = {'name':restaurant_name, 'address':restaurant_address, 'image':imageURL}
+        return restaurantInfo
+    else:
+        return "No Restaurants Found"
